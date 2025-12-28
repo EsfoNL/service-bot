ff472ee43c608e5cc9b96a2a3c2b0a7ce9aa593f
Ersa <esfonl@esfokk.nl>
2025-12-29 00:20:20 +0100

    different allowed services per server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::{sync::LazyLock, thread::sleep, time::Duration};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::Write,
+    ops::DerefMut,
+    path::{Path, PathBuf},
+    sync::{LazyLock, Mutex, RwLock},
+    thread::sleep,
+    time::Duration,
+};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serenity::all::{
     Activity, ActivityData, ClientBuilder, Command, CommandInteraction, CommandOptionType,
-    CommandType, Context, CreateCommand, CreateCommandOption, CreateInteractionResponseMessage,
-    EventHandler, GatewayIntents, Interaction, Ready,
+    CommandType, Context, CreateAutocompleteResponse, CreateCommand, CreateCommandOption,
+    CreateInteractionResponseMessage, EventHandler, GatewayIntents, Interaction, Ready,
 };
 
 #[derive(Deserialize)]
 struct Config {
     token: String,
-    services: Vec<String>,
 }
 
 const SERVICE_ACTIONS: [&str; 3] = ["start", "stop", "restart"];
@@ -41,11 +49,22 @@ fn get_args(cmd: &CommandInteraction) -> Option<(&str, &str)> {
         .map(|e| (e.0.value.as_str().unwrap(), e.1.value.as_str().unwrap()))
 }
 
-fn check_status() -> String {
+fn allowed_service(id: u64, service: &str) -> bool {
+    let lock = SERVERS_CFG.read().unwrap();
+    // dbg!(id, &lock, service);
+    lock.servers_services
+        .get(&id)
+        .is_some_and(|e| e.iter().any(|e| e == service))
+}
+
+fn check_status(guild_id: u64) -> String {
     use std::fmt::Write;
     let mut out = String::new();
-    out.clear();
-    for service in CFG.services.iter() {
+    let lock = SERVERS_CFG.read().unwrap();
+    let Some(services) = lock.servers_services.get(&guild_id) else {
+        return String::new();
+    };
+    for service in services {
         let status = std::process::Command::new("systemctl")
             .args(["is-active", service])
             .status()
@@ -80,18 +99,15 @@ impl EventHandler for Handler {
                     }
                     opt
                 })
-                .add_option({
-                    let mut opt = CreateCommandOption::new(
+                .add_option(
+                    CreateCommandOption::new(
                         CommandOptionType::String,
                         "service",
                         "the service in question",
                     )
-                    .required(true);
-                    for i in CFG.services.iter() {
-                        opt = opt.add_string_choice(i, i);
-                    }
-                    opt
-                })
+                    .required(true)
+                    .set_autocomplete(true),
+                )
                 .description("manages server services")
                 .kind(CommandType::ChatInput),
         )
@@ -105,6 +121,23 @@ impl EventHandler for Handler {
         )
         .await
         .expect("failed to create command");
+
+        Command::create_global_command(
+            &ctx,
+            CreateCommand::new("add_service")
+                .kind(CommandType::ChatInput)
+                .description("admin thing")
+                .add_option(
+                    CreateCommandOption::new(
+                        CommandOptionType::String,
+                        "service",
+                        "which service to add",
+                    )
+                    .required(true),
+                ),
+        )
+        .await
+        .expect("failed to create command");
     }
 
     async fn interaction_create(
@@ -113,6 +146,30 @@ impl EventHandler for Handler {
         interaction: serenity::all::Interaction,
     ) {
         match interaction {
+            Interaction::Autocomplete(cmd) => {
+                if cmd.data.autocomplete().unwrap().name == "service" {
+                    let _ = cmd
+                        .create_response(
+                            &ctx,
+                            serenity::all::CreateInteractionResponse::Autocomplete({
+                                let mut opts = CreateAutocompleteResponse::new();
+                                let lock = SERVERS_CFG.read().unwrap();
+                                let Some(services) =
+                                    lock.servers_services.get(&cmd.guild_id.unwrap().get())
+                                else {
+                                    return;
+                                };
+                                for service in services {
+                                    opts = opts.add_string_choice(service, service);
+                                }
+
+                                opts
+                            }),
+                        )
+                        .await;
+                }
+            }
+
             // serenity::all::InteractionType::Ping => todo!(),
             Interaction::Command(cmd) => match cmd.data.name.as_str() {
                 "service" => {
@@ -120,8 +177,8 @@ impl EventHandler for Handler {
                         let Some((action, service)) = get_args(&cmd) else {
                             return Err("Invalid args".to_string());
                         };
-                        if !SERVICE_ACTIONS.contains(&action)
-                            || !CFG.services.iter().any(|e| e == service)
+                        if !(SERVICE_ACTIONS.contains(&action)
+                            && allowed_service(cmd.guild_id.unwrap().get(), service))
                         {
                             return Err("Invalid service or action".to_string());
                         };
@@ -147,7 +204,54 @@ impl EventHandler for Handler {
                             ctx,
                             serenity::all::CreateInteractionResponse::Message(
                                 CreateInteractionResponseMessage::new()
-                                    .content(check_status())
+                                    .content(check_status(cmd.guild_id.unwrap().get()))
+                                    .ephemeral(true),
+                            ),
+                        )
+                        .await;
+                }
+                "add_service" => {
+                    let Some(service) = cmd
+                        .data
+                        .options
+                        .first()
+                        .and_then(|e| e.value.as_str().filter(|_| e.name == "service"))
+                    else {
+                        return;
+                    };
+
+                    let guild_id = cmd.guild_id.unwrap().get();
+                    {
+                        let mut lock = SERVERS_CFG.write().unwrap();
+
+                        lock.servers_services
+                            .entry(guild_id)
+                            .or_default()
+                            .push(service.to_string());
+                    }
+                    {
+                        let mut path = String::from(server_cfg_location());
+                        path += ".tmp";
+                        let mut file = File::create(&path).unwrap();
+
+                        file.write_all(
+                            toml::to_string_pretty(&*SERVERS_CFG.read().unwrap())
+                                .unwrap()
+                                .as_bytes(),
+                        )
+                        .unwrap();
+                        drop(file);
+                        std::fs::rename(&path, server_cfg_location()).unwrap();
+                    }
+
+                    let _ = cmd
+                        .create_response(
+                            ctx,
+                            serenity::all::CreateInteractionResponse::Message(
+                                CreateInteractionResponseMessage::new()
+                                    .content(format!(
+                                        "added {service} to allowed services for this server"
+                                    ))
                                     .ephemeral(true),
                             ),
                         )
@@ -174,6 +278,26 @@ static CFG: LazyLock<Config> = LazyLock::new(|| {
         .unwrap(),
     )
     .unwrap()
+});
+
+#[derive(Deserialize, Serialize, Default, Clone, Debug)]
+struct ServersCfg {
+    servers_services: HashMap<u64, Vec<String>>,
+}
+
+const fn server_cfg_location() -> &'static str {
+    if cfg!(debug_assertions) {
+        "dev.servers.toml"
+    } else {
+        "servers.toml"
+    }
+}
+static SERVERS_CFG: LazyLock<RwLock<ServersCfg>> = LazyLock::new(|| {
+    toml::from_str::<ServersCfg>(
+        &std::fs::read_to_string(server_cfg_location()).unwrap_or_default(),
+    )
+    .unwrap_or_default()
+    .into()
 });
 
 #[tokio::main]
